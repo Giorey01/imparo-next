bdeaecfb88539ad4eceb9b7c34e8bb4c2396ceb5
Giorey01 <giorey2001@gmail.com>
2024-05-28 22:38:08 +0200

    Not found page

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notfound() {
+  return (
+    <h2>Review not found</h2>
+  )
+}
+
+export default Notfound
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { notFound } from 'next/navigation'; 
 
 function ReviewDetail( { params }: {
     params: {
@@ -6,6 +7,11 @@ function ReviewDetail( { params }: {
         reviewId: string;
     }
 }) {
+
+    if(parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
+
   return (
     <h1> Review { params.reviewId } for product { params.productId }</h1>
   )
